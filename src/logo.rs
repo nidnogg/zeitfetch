1f24e7814f23b65c4acd2b59d45e470036b3547d
nidnogg <henriquevt98@gmail.com>
2022-09-06 08:51:30 -0400

    Added fedora ASCII

--- src/logo.rs
+++ src/logo.rs
@@ -45,7 +45,34 @@ pub fn get_logo_by_distro(sys_name: &str) -> String {
 \x1b[1m              \x1b[91;1m/osyyyyyyo\x1b[93;1m++ooo+++/\x1b[0m
 \x1b[1m                  \x1b[91;1m`````\x1b[0m \x1b[93;1m+oo+++o\\:\x1b[0m
 \x1b[1m                         \x1b[93;1m`oo++.\x1b[0m
+                ",
+            );
+            logo
+        }
+        "fedora" => {
+            let logo = String::from(
                 "
+            
+            .',;::::;,'.               
+            .';:cccccccccccc:;,.            
+         .;cccccccccccccccccccccc;.        
+       .:cccccccccccccccccccccccccc:.        
+     .;ccccccccccccc;.:dddl:.;ccccccc;.     
+    .:ccccccccccccc;OWMKOOXMWd;ccccccc:.    
+   .:ccccccccccccc;KMMc;cc;xMMc:ccccccc:.   
+   ,cccccccccccccc;MMM.;cc;;WW::cccccccc,   
+   :cccccccccccccc;MMM.;cccccccccccccccc:   
+   :ccccccc;oxOOOo;MMM0OOk.;cccccccccccc:  
+   cccccc:0MMKxdd:;MMMkddc.;cccccccccccc; 
+   ccccc:XM0';cccc;MMM.;cccccccccccccccc'   
+   ccccc;MMo;ccccc;MMW.;ccccccccccccccc;    
+   ccccc;0MNc.ccc.xMMd:ccccccccccccccc;     
+   cccccc;dNMWXXXWM0::cccccccccccccc:,      
+   cccccccc;.:odl:.;cccccccccccccc:,.        
+   :cccccccccccccccccccccccccccc:'.       
+   .:cccccccccccccccccccccc:;,..
+     '::cccccccccccccc::;,.        
+     ",
             );
             logo
         }
